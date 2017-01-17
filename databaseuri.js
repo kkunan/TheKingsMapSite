e479def83815b3be98cb33bd1f5ace7217265504
Suppassara K <tangmae@users.noreply.github.com>
2017-01-17 17:05:24 +0900

    Update databaseuri.js

--- databaseuri.js
+++ databaseuri.js
@@ -1,2 +1,2 @@
 // exports.database = 'mongodb://localhost:27017/contents' //local
-exports.database = 'mongodb://site:9ramaking@ds045031.mlab.com:45031/heroku_2khfgvtb' //mongolab
+//exports.database = 'mongodb://site:9ramaking@___.mlab.com:45031/heroku_2khfgvtb' //mongolab
