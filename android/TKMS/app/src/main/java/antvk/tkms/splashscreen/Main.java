2512eafc40b8b6e8773c92d496f56f83cc4992c0
Auxtopuz <auxtopuz@gmail.com>
2018-05-06 21:10:33 +0700

    Splash

---
+++ android/TKMS/app/src/main/java/antvk/tkms/splashscreen/Main.java
@@ -0,0 +1,16 @@
+package antvk.tkms.splashscreen;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+import antvk.tkms.R;
+
+public class Main extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_maps);
+    }
+}
---
+++ android/TKMS/app/src/main/java/antvk/tkms/splashscreen/Splash.java
@@ -0,0 +1,44 @@
+package antvk.tkms.splashscreen;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+import antvk.tkms.R;
+
+public class Splash extends Activity {
+    Handler handler;
+    Runnable runnable;
+    long delay_time;
+    long time = 3000L;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.splashscreen);
+        handler = new Handler();
+
+        runnable = new Runnable() {
+            public void run() {
+                Intent intent = new Intent(Splash.this, Main.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+    }
+
+    public void onResume() {
+        super.onResume();
+        delay_time = time;
+        handler.postDelayed(runnable, delay_time);
+        time = System.currentTimeMillis();
+    }
+
+    public void onPause() {
+        super.onPause();
+        handler.removeCallbacks(runnable);
+        time = delay_time - (System.currentTimeMillis() - time);
+    }
+}
