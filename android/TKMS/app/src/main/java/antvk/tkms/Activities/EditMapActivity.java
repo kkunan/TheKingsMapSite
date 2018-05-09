6f8bab917fe22085c791de3608bd0a70e0ec76af
Kamolwan Kunanusont <kkunan@essex.ac.uk>
2018-05-09 12:59:06 +0100

    layout หน้าเพิ่ม map, แก้ place, แก้ event เสร็จ เหลือแต่ java กับแก้รูปปุ่มต่าง ๆ

--- android/TKMS/app/src/main/java/antvk/tkms/Activities/EditMapActivity.java
+++ android/TKMS/app/src/main/java/antvk/tkms/Activities/EditMapActivity.java
@@ -2,11 +2,22 @@ package antvk.tkms.Activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class EditMapActivity extends AppCompatActivity{
+import antvk.tkms.R;
+
+public class EditMapActivity extends ActivityWithBackButton{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_map);
+
+
+    }
+
+    public void onSubmitButtonClick(View view)
+    {
+
     }
 }
