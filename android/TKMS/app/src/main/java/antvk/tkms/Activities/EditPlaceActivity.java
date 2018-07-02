625952bc7ea01b7af6dbda51215d0d13a450cbb9
Kamolwan Kunanusont <kkunan@essex.ac.uk>
2018-07-02 12:56:12 +0100

    เพิ่ม place radius setup

--- android/TKMS/app/src/main/java/antvk/tkms/Activities/EditPlaceActivity.java
+++ android/TKMS/app/src/main/java/antvk/tkms/Activities/EditPlaceActivity.java
@@ -1,7 +1,10 @@
 package antvk.tkms.Activities;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationManager;
 import android.net.Uri;
@@ -9,11 +12,14 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ScrollView;
 import android.widget.Spinner;
 
 import com.google.android.gms.common.api.Status;
@@ -25,10 +31,15 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.gson.reflect.TypeToken;
+import com.google.maps.android.SphericalUtil;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -44,6 +55,7 @@ import antvk.tkms.Utils.ClassMapper;
 import antvk.tkms.Utils.ImageUtils;
 import antvk.tkms.Utils.LocationUtils;
 import antvk.tkms.Utils.UIUtils;
+import antvk.tkms.ViewManager.CustomScrollView;
 
 import static antvk.tkms.Activities.MapSelectorActivity.*;
 
@@ -53,11 +65,14 @@ public class EditPlaceActivity extends AddStuffsActivity implements OnMapReadyCa
     PlaceAutocompleteFragment autocompleteFragment;
 
     GoogleMap gMap;
-    Marker currentPlaceMarker;
+    Marker currentPlaceMarker, radiusPickerMarker;
+    Circle circleRadius;
+    boolean dragMode = false;
 
     Spinner dropdown;
     EditText descriptionEditText, placeNameEditText;
     ImageView placeImageView;
+    CustomScrollView scrollView;
 //    EventViewAdapter adapter;
 
     Bundle b = new Bundle();
@@ -77,6 +92,8 @@ public class EditPlaceActivity extends AddStuffsActivity implements OnMapReadyCa
         descriptionEditText = (EditText) findViewById(R.id.place_description_edittext);
         placeNameEditText = (EditText) findViewById(R.id.place_name_edittext);
         placeImageView = findViewById(R.id.add_map_image_button);
+
+        scrollView = findViewById(R.id.scrollView);
 
         if (currentItem == null) {
             currentItem = new PlaceItem();
@@ -139,12 +156,48 @@ public class EditPlaceActivity extends AddStuffsActivity implements OnMapReadyCa
             public void onPlaceSelected(Place place) {
                 if(currentItem==null)
                     currentItem = new PlaceItem();
+
                 currentItem.placeID = place.getId();
                 currentItem.location = place.getLatLng();
                 currentItem.placeRealName = place.getName().toString();
                 if (gMap != null) {
                     currentPlaceMarker = setMarker(currentItem.header, currentItem.location);
-                    CameraUpdate cameraUpdateFactory = CameraUpdateFactory.newLatLngZoom(currentPlaceMarker.getPosition(), Constants.STREET_LEVEL_ZOOM);
+
+                    if (radiusPickerMarker != null) {
+                        radiusPickerMarker.remove();
+                    }
+
+                    LatLng newPosition = SphericalUtil.computeOffset(currentPlaceMarker.getPosition(), currentItem.radius, 90);
+                    MarkerOptions markerOptions = new MarkerOptions()
+                            .icon(BitmapDescriptorFactory.fromResource(R.mipmap.add_map_icon))
+                            .position(newPosition)
+                            .draggable(true)
+                            ;
+                    radiusPickerMarker = gMap.addMarker(markerOptions);
+
+                    if (circleRadius != null)
+                    {
+                        circleRadius.remove();
+                    }
+
+                    circleRadius = gMap.addCircle(new CircleOptions()
+                            .radius(currentItem.radius)
+                            .center(currentPlaceMarker.getPosition())
+                            .strokeColor(Color.RED)
+                            .fillColor(0x220000FF)
+                            .strokeWidth(5)
+                    );
+
+                    LatLngBounds.Builder latlngBuilder = new LatLngBounds.Builder();
+                    LatLng left = SphericalUtil.computeOffset(currentPlaceMarker.getPosition(), currentItem.radius*2, 45);
+                    LatLng up = SphericalUtil.computeOffset(currentPlaceMarker.getPosition(), currentItem.radius*2, 225);
+
+
+                    latlngBuilder.include(newPosition);
+                    latlngBuilder.include(left);
+                    latlngBuilder.include(up);
+
+                    CameraUpdate cameraUpdateFactory = CameraUpdateFactory.newLatLngBounds(latlngBuilder.build(),50);
                     gMap.moveCamera(cameraUpdateFactory);
                 }
 
@@ -254,11 +307,104 @@ public class EditPlaceActivity extends AddStuffsActivity implements OnMapReadyCa
                     MapsActivity.MY_PERMISSIONS_REQUEST_FINE_LOCATION);
             return;
         }
+        gMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                if(marker.equals(radiusPickerMarker) && !dragMode)
+                {
+                    enableDragMode();
+                    return false;
+                }
+                else if(marker.equals(currentPlaceMarker) && dragMode)
+                {
+                    disableDragMode();
+                    return false;
+                }
+                return true;
+            }
+        });
         gMap.setMyLocationEnabled(true);
+        gMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+            @Override
+            public void onMarkerDragStart(Marker marker) {
+                System.out.println("drag start!");
+            }
+
+            @Override
+            public void onMarkerDrag(Marker marker) {
+                if(marker.equals(radiusPickerMarker) && dragMode)
+                {
+                    double dist = SphericalUtil.computeDistanceBetween(radiusPickerMarker.getPosition(),currentPlaceMarker.getPosition());
+
+                    if(dist<MarkerEventListActivity.CHECKIN_AVALABLE_RANGE) {
+                        marker.setPosition(
+                                SphericalUtil.computeOffset(currentPlaceMarker.getPosition(),
+                                        MarkerEventListActivity.CHECKIN_AVALABLE_RANGE,
+                                        90
+                                )
+                        );
+                    }
+
+                    else
+                        circleRadius.setRadius(dist);
+
+
+                }
+            }
+
+            @Override
+            public void onMarkerDragEnd(Marker marker) {
+                if(marker.equals(radiusPickerMarker)) {
+                    currentItem.radius = SphericalUtil.computeDistanceBetween(radiusPickerMarker.getPosition(), currentPlaceMarker.getPosition());
+
+                    LatLng left = SphericalUtil.computeOffset(currentPlaceMarker.getPosition(), currentItem.radius*2, 45);
+                    LatLng up = SphericalUtil.computeOffset(currentPlaceMarker.getPosition(), currentItem.radius*2, 225);
+
+                    LatLngBounds.Builder latlngBuilder = new LatLngBounds.Builder();
+
+                    latlngBuilder.include(left);
+                    latlngBuilder.include(up);
+
+                    CameraUpdate cameraUpdateFactory = CameraUpdateFactory.newLatLngBounds(latlngBuilder.build(),50);
+                    gMap.moveCamera(cameraUpdateFactory);
+                }
+            }
+        });
+
 
         if(currentItem.location!=null) {
             currentPlaceMarker = setMarker(currentItem.header,currentItem.location);
-            CameraUpdate cameraUpdateFactory = CameraUpdateFactory.newLatLngZoom(currentPlaceMarker.getPosition(), Constants.STREET_LEVEL_ZOOM);
+
+            if(circleRadius!=null)
+                circleRadius.remove();
+
+            circleRadius = gMap.addCircle(new CircleOptions()
+                    .radius(currentItem.radius)
+                    .center(currentPlaceMarker.getPosition())
+                    .strokeColor(Color.RED)
+                    .fillColor(0x220000FF)
+                    .strokeWidth(5))
+            ;
+            LatLng newPosition = SphericalUtil.computeOffset(currentPlaceMarker.getPosition(), currentItem.radius, 90);
+            MarkerOptions markerOptions = new MarkerOptions()
+                    .icon(BitmapDescriptorFactory.fromResource(R.mipmap.add_map_icon))
+                    .position(newPosition)
+                    .draggable(true)
+                    ;
+
+            if(radiusPickerMarker!=null)
+                radiusPickerMarker.remove();
+            radiusPickerMarker = gMap.addMarker(markerOptions);
+
+            LatLngBounds.Builder latlngBuilder = new LatLngBounds.Builder();
+            LatLng left = SphericalUtil.computeOffset(currentPlaceMarker.getPosition(), currentItem.radius*2, 45);
+            LatLng up = SphericalUtil.computeOffset(currentPlaceMarker.getPosition(), currentItem.radius*2, 225);
+
+            latlngBuilder.include(newPosition);
+            latlngBuilder.include(left);
+            latlngBuilder.include(up);
+
+            CameraUpdate cameraUpdateFactory = CameraUpdateFactory.newLatLngBounds(latlngBuilder.build(),50);
             gMap.moveCamera(cameraUpdateFactory);
         }
 
@@ -276,8 +422,66 @@ public class EditPlaceActivity extends AddStuffsActivity implements OnMapReadyCa
         }
         gMap.setOnMapLongClickListener(latLng -> {
             // TODO: 01/07/2018 reverse geolocation if possible
-            currentPlaceMarker = setMarker("random spot", latLng);
+            if(!dragMode) {
+
+                currentPlaceMarker = setMarker("random spot", latLng);
+
+                if(circleRadius!=null)
+                    circleRadius.remove();
+
+                circleRadius = gMap.addCircle(new CircleOptions()
+                        .radius(currentItem.radius)
+                        .center(currentPlaceMarker.getPosition())
+                        .strokeColor(Color.RED)
+                        .fillColor(0x220000FF)
+                        .strokeWidth(5))
+                        ;
+                LatLng newPosition = SphericalUtil.computeOffset(currentPlaceMarker.getPosition(), currentItem.radius, 90);
+                MarkerOptions markerOptions = new MarkerOptions()
+                        .icon(BitmapDescriptorFactory.fromResource(R.mipmap.add_map_icon))
+                        .position(newPosition)
+                        .draggable(true)
+                        ;
+
+                if(radiusPickerMarker!=null)
+                    radiusPickerMarker.remove();
+                radiusPickerMarker = gMap.addMarker(markerOptions);
+
+                LatLngBounds.Builder latlngBuilder = new LatLngBounds.Builder();
+                LatLng left = SphericalUtil.computeOffset(currentPlaceMarker.getPosition(), currentItem.radius*2, 45);
+                LatLng up = SphericalUtil.computeOffset(currentPlaceMarker.getPosition(), currentItem.radius*2, 225);
+
+                latlngBuilder.include(newPosition);
+                latlngBuilder.include(left);
+                latlngBuilder.include(up);
+
+                CameraUpdate cameraUpdateFactory = CameraUpdateFactory.newLatLngBounds(latlngBuilder.build(),50);
+                gMap.animateCamera(cameraUpdateFactory);
+            }
         });
+    }
+
+    private void enableDragMode() {
+        gMap.getUiSettings().setScrollGesturesEnabled(false);
+        gMap.getUiSettings().setZoomControlsEnabled(false);
+        gMap.getUiSettings().setTiltGesturesEnabled(false);
+        dragMode = true;
+        scrollView.setEnableScrolling(false);
+        circleRadius.setStrokeColor(Color.GREEN);
+        currentPlaceMarker.setIcon(BitmapDescriptorFactory.fromResource(R.mipmap.tick_icon));
+        circleRadius.setFillColor(0x2200FF00);
+    }
+
+    private void disableDragMode(){
+
+        gMap.getUiSettings().setScrollGesturesEnabled(true);
+        gMap.getUiSettings().setZoomControlsEnabled(true);
+        gMap.getUiSettings().setTiltGesturesEnabled(true);
+        dragMode = false;
+        scrollView.setEnableScrolling(true);
+        currentPlaceMarker.setIcon(BitmapDescriptorFactory.defaultMarker());
+        circleRadius.setStrokeColor(Color.RED);
+        circleRadius.setFillColor(0x220000FF);
     }
 
     public Marker setMarker(String header, LatLng location)
@@ -305,6 +509,10 @@ public class EditPlaceActivity extends AddStuffsActivity implements OnMapReadyCa
 
         if(currentItem==null)
             currentItem = new PlaceItem();
+
+        if(currentItem.radius < MarkerEventListActivity.CHECKIN_AVALABLE_RANGE)
+            currentItem.radius = MarkerEventListActivity.CHECKIN_AVALABLE_RANGE;
+
         currentItem.header = placeNameEditText.getText().toString();
         currentItem.placeCategory = placeCategories.get(dropdown.getSelectedItemPosition());
 
@@ -364,5 +572,6 @@ public class EditPlaceActivity extends AddStuffsActivity implements OnMapReadyCa
     @Override
     public void selectPicAction(String picturePath) {
     }
+
 
 }
